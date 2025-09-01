ea0d0e761f8ea99fc2533d07e214d67c1cf2fd0c
PauravThakkar <paurav.t20@gmail.com>
2025-08-31 21:46:38 -0400

    parallax  scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Navbar from "./Components/Navbar";
 import Image from "next/image";
+import styles from "./styles.module.scss"
 
 export default function Home() {
   return (
     <>
-    <div className="page">
+    <div className={`${styles.home} page`}>
       <Navbar>
       </Navbar>
+      <div className={styles.homeBanner}></div>
+      <div className={styles.dummyContent}>
+        This is the dummy content to test the parallax scroll
+      </div>
     </div>
     </>
   );
